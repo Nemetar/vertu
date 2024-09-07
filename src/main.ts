ca574910c907a7b90d49a2eafbb597c4585c8b5d
Mashiba <tnairi06@outlook.fr>
2024-09-07 14:52:24 +0200

    [/] set pinia persistent store

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './app/router/router';
+import pinia from './app/store/store';
 import registerComponents from './app/components/components';
 import 'virtual:uno.css';
 
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
